983dfd2553210f7f047ec78003a255a682de1b2b
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-01-09 14:06:02 +0530

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,22 +1,3 @@
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-
-// const firebaseConfig2 = {
-//   apiKey: "AIzaSyC1mU4cGi0kxGmAjzvA-zrkGVbXqfMQBtQ",
-//   authDomain: "krashak-setu-testing.firebaseapp.com",
-//   projectId: "krashak-setu-testing",
-//   storageBucket: "krashak-setu-testing.appspot.com",
-//   messagingSenderId: "730432132265",
-//   appId: "1:730432132265:web:eb84541bbc422dfa1705b8",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig2);
-
-// // Initialize Firestore
-// export const db = getFirestore(app);
-
-// Import necessary Firebase modules
 // firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
